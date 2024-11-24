ce0006d6a34e624c0bb46d33951fb26f3c4a7243
Josh Garverick <jgarverick@github.com>
2024-11-23 22:28:33 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo '[INFO] Setting up Powerlevel10k...'
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+echo '[INFO] Setting up git aliases...'
+git config --global alias.lol "log --oneline --graph --decorate --all"
+echo '[INFO] Do not forget to set your user name and email in the git config!'
+
+echo '[INFO] Setting up oh-my-zsh...'
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
+
+exec zsh
