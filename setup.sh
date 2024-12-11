b5baa15b4ddbf3d6208b7e5b0f3ba2b7749a26bc
Josh Garverick <jgarverick@github.com>
2024-12-10 20:56:58 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,5 @@ echo '[INFO] Do not forget to set your user name and email in the git config!'
 echo '[INFO] Copying p10k.zsh...'
 cp .p10k.zsh ~/.p10k.zsh
 
-echo '[INFO] Setting up oh-my-zsh...'
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
+echo '[INFO] Setup of oh-my-zsh complete.'
 
